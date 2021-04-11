00f73ad801b25f9b851ece456f9abe2c5253e4ae
Fajri Pitutur Jati <fajripitutur@gmail.com>
2021-04-11 14:50:19 +0700

    adding orches redis

--- server/orchestrator/app.js
+++ server/orchestrator/app.js
@@ -1,9 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require("axios");
 const Redis = require("ioredis");
-const { update } = require('../orchestrator-express/controllers/moviesController');
 const redis = new Redis();
 const movieURL = "http://localhost:4001/movies/"
+const seriesURL = "http://localhost:4002/tvseries/"
 
 const typeDefs = gql`
 
@@ -20,8 +20,16 @@ const typeDefs = gql`
     message: String
   }
 
-  type TvSeries {
-    id: ID
+  input MovieInput {
+    title: String
+    overview: String
+    poster_path: String
+    popularity: Float
+    tags: [String] 
+  }
+
+  type Series {
+    _id: ID
     title: String
     overview: String
     poster_path: String
@@ -29,16 +37,33 @@ const typeDefs = gql`
     tags: [String]
   }
 
+  type SeriesMessage {
+    message: String
+  }
+
+  input SeriesInput {
+    title: String
+    overview: String
+    poster_path: String
+    popularity: Float
+    tags: [String] 
+  }
+
   type Query {
     movies: [Movie]
     movie(_id: ID): Movie
-    tvseries: [TvSeries]
+    series: [Series]
+    seri(_id: ID) : Series
   }
 
   type Mutation {
     createMovie(title: String, overview: String, poster_path: String, popularity: Float, tags: [String]) : Movie
     updateMovie(_id: ID, title: String, overview: String, poster_path: String, popularity: Float, tags: [String]) : MovieMessage
     deleteMovie(_id: ID): MovieMessage
+
+    createSeries(title: String, overview: String, poster_path: String, popularity: Float, tags: [String]) : Series
+    updateSeries(_id: ID, title: String, overview: String, poster_path: String, popularity: Float, tags: [String]) : SeriesMessage
+    deleteSeries(_id: ID): SeriesMessage
   }
 `;
 
@@ -79,7 +104,43 @@ const resolvers = {
       } catch (error) {
         console.log(error, '========');
       }
-    }
+    }, 
+
+    series: async () => {
+      try {
+        let seriesRedis = await redis.get('series:alldata')
+        if (!seriesRedis) {
+
+          const { data } = await axios.get(seriesURL)
+
+          redis.set('series:alldata', JSON.stringify(data))
+          return data
+        } else {
+          return JSON.parse(seriesRedis)
+        }
+      } catch (error) {
+        return error
+      }
+    },
+
+    seri: async (_, args) => {
+      const {_id} = args
+      try {
+        let seriesOneRedis = await redis.get(`series:onedata${_id}`)
+        if (!seriesOneRedis) {
+
+          const { data } = await axios.get(`${seriesURL}${_id}`)
+
+          redis.set(`series:onedata${_id}`, JSON.stringify(data))
+          return data
+        } else {
+          return JSON.parse(seriesOneRedis)
+        }
+      } catch (error) {
+        console.log(error, '========');
+        return error
+      }
+    },
   },
 
   Mutation: {
@@ -97,6 +158,23 @@ const resolvers = {
         const { data } = await axios.post(movieURL, {...newMovie});
         return data
 
+      } catch (error) {
+        return error
+      }
+    },
+
+    createSeries: async (_, args) => {
+      try {
+        const newSeries = {
+          title: args.title,
+          overview: args.overview,
+          poster_path: args.poster_path,
+          popularity: args.popularity,
+          tags: args.tags,
+        }
+        redis.del("series:alldata")
+        const { data } = await axios.post(seriesURL, {...newSeries});
+        return data
       } catch (error) {
         return error
       }
@@ -121,6 +199,25 @@ const resolvers = {
       }
     },
 
+    updateSeries: async(_, args) => {
+      const {_id} = args
+      const updateSeries = {
+        title: args.title,
+        overview: args.overview,
+        poster_path: args.poster_path,
+        popularity: args.popularity,
+        tags: args.tags,
+      }
+      try {
+        const { data } = await axios.put(`${seriesURL}${_id}`, {...updateSeries})
+        redis.del("movies:alldata")
+        redis.del(`movies:onedata${_id}`)        
+        return data
+      } catch (error) {
+        return error
+      }
+    },
+
     deleteMovie: async(_, args) => {
       try {
         const {_id} = args
@@ -132,7 +229,19 @@ const resolvers = {
 
         return error
       }
-    }
+    },
+    deleteSeries: async(_, args) => {
+      try {
+        const {_id} = args
+        const {data} = await axios.delete(`${seriesURL}${_id}`)
+        redis.del("series:alldata")
+        redis.del(`series:onedata${_id}`)
+        return data
+      } catch (error) {
+
+        return error
+      }
+    },
   }
 };
 
